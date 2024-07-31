3e7a78c7540385e0fd600b748a7268cb9c0b986e
Stanislav Krymniak <kryminak.stas@gmail.com>
2024-07-31 17:35:39 +0200

    commitingchanges

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -14,11 +14,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <Elements stripe={stripePromise}>
             <App />
           </Elements>
         </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- react-app/src/store/store.js
+++ react-app/src/store/store.js
@@ -4,11 +4,11 @@ import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
-
+/*
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
-/*
+
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
     window &&

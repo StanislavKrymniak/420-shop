124326838a9b9daa4eb7acbe3e03c0ea14b0c30e
Stanislav Krymniak <kryminak.stas@gmail.com>
2024-10-24 19:32:35 +0200

    commiting changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Shop from './routes/shop/shop.component';
